ac287c03205cb84a4b99eaba25f781f487cc9f13
vlad <vlad-shev@ukr.net>
2018-05-19 17:41:33 +0300

    user app

--- joinme/joinme/settings.py
+++ joinme/joinme/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
